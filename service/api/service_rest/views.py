ea7793c3c2004da3022d4cba789366b6be4578de
Lauren Lowe <lauren.lowe84@gmail.com>
2022-12-07 11:30:42 -0800

    Service_rest models created

--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
+from common.json import ModelEncoder
+from .models import AutomobileVO, Technician, Appointment
 
 # Create your views here.
+
+
+api_list_techs
+
+api_list_appts
