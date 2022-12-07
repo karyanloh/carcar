52d6cb9d9b4d108cb18ce27a2ddf187018beaf62
stephanie <karyansloh@gmail.com>
2022-12-06 16:05:05 -0800

    added routes

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -2,13 +2,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
 
-function App() {
+function App(props) {
+
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="manufacturer" element={<Manufacturers manufacturer={props.manufacturer} />} />
+          <Route path="vehicles" element={<Vehicles vehicleModel={props.vehicleModel} />} />
+          <Route path="automobile" element={<Automobiles automobile={props.automobile} />} />
+          <Route path="manufacturer/new" element={<ManufacturerForm manufacturer={props.manufacturer}/>}/>
+          <Route path= "vehicles/new" element = {<VehicleForm vehicleModel={props.vehicleModel}/>}/>
+          <Route path= "automobile/new" element = {<AutomobileForm automobile={props.automobile}/>}/>
         </Routes>
       </div>
     </BrowserRouter>
